859b917ab5884ef923d578972c040534426e6cd6
swethabammidi <online.swetha@gmail.com>
2023-02-16 19:23:52 +0530

    Create run-import.sh

---
+++ run-import.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+# Define variables
+export URL="http://3.108.44.52:8080/api/v2/import-scan/"
+export TOKEN="e596a9d6f75615c59f0a80b4afa7acf695e8df61"
+export SEVERITY="Info"
+export #SCAN_TYPE="oast-results Scan"
+export #FILE_PATH="oast-results.json"
+export PRODUCT_NAME="DevSecOps pipeline reports"
+export ENGAGEMENT_NAME="Vulnerability assessment"
+export ENGAGEMENT_ID="1"
+
+curl -X 'POST' \
+  'http://3.108.44.52:8080/api/v2/import-scan/' \
+  -H 'accept: application/json' \
+  -H 'Content-Type: multipart/form-data' \
+  -H 'Authorization: Token e596a9d6f75615c59f0a80b4afa7acf695e8df61' \
+  -F 'minimum_severity=Info' \
+  -F 'active=true' \
+  -F 'verified=true' \
+  -F 'scan_type=Bandit Scan' \
+  -F 'file=@bandit-results.json;type=application/json' \
+  -F 'product_name=DevSecOps pipeline reports' \
+  -F 'engagement_name=Bandit scan' \
+  -F 'engagement=3' \
+  -F 'auto_create_context=true' \
+  -F 'close_old_findings=false' \
+  -F 'close_old_findings_product_scope=false' \
+  -F 'push_to_jira=false' \
+  -F 'create_finding_groups_for_all_findings=true'
+
+curl -X 'POST' \
+  'http://3.108.44.52:8080/api/v2/import-scan/' \
+  -H 'accept: application/json' \
+  -H 'Content-Type: multipart/form-data' \
+  -H 'Authorization: Token e596a9d6f75615c59f0a80b4afa7acf695e8df61' \
+  -F 'minimum_severity=Info' \
+  -F 'active=true' \
+  -F 'verified=true' \
+  -F 'scan_type=Retire.js Scan' \
+  -F 'file=@retirejs-report.json;type=application/json' \
+  -F 'product_name=DevSecOps pipeline reports' \
+  -F 'engagement_name=Retire.js Scan' \
+  -F 'engagement=4' \
+  -F 'close_old_findings=false' \
+  -F 'close_old_findings_product_scope=false' \
+  -F 'push_to_jira=false' \
+  -F 'create_finding_groups_for_all_findings=true'
